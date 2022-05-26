13f4af4ec2603f7450bb27fbff81aec5b23bdf11
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2022-05-26 23:25:32 +0200

    Add files via upload

---
+++ components/Check/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import {
+  Col,
+  Container,
+  FormSelect,
+  Image,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Button,
+  Row,
+} from "react-bootstrap";
+
+export default function Check() {
+  return <div>CureDOktorcinoJedna</div>;
+}
---
+++ components/Footer/index.js
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import {
+  Col,
+  Container,
+  FormSelect,
+  Image,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Button,
+  Row,
+} from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <div>
+      <main className={styles.main}></main>
+      <footer className={styles.footer}>
+        <div className="container">
+          <div className={styles.imageContainer}>
+            <Image
+              src="/assets/homepage/logo-footer.png"
+              height={45}
+              width={210}
+              fluid
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.telephone}>
+            <p>QUESTIONS?</p>
+            <Link href="tel:1-800-234-7654">
+              <a className={styles.number}>1-800-234-7654</a>
+            </Link>
+          </div>
+          <Container>
+            <Row className={styles.footerRow}>
+              <div className={styles.links}>
+                <Link href="/home">
+                  <a>Home</a>
+                </Link>
+                <Link href="/about">
+                  <a>About Us</a>
+                </Link>
+                <Link href="/faq">
+                  <a>FAQs</a>
+                </Link>
+                <Link href="/contact">
+                  <a>Contact Us</a>
+                </Link>
+                <Link href="/login">
+                  <a>LOGIN</a>
+                </Link>
+                <Link href="/telehealth">
+                  <a>Telehealth</a>
+                </Link>
+                <Link href="/terms">
+                  <a>Terms of Use</a>
+                </Link>
+                <Link href="/privacy">
+                  <a>Privacy Policy</a>
+                </Link>
+              </div>
+            </Row>
+            <p className={styles.copyright}>
+              PureBlue. © 2022. All Rights Reserved
+            </p>
+          </Container>
+        </div>
+      </footer>
+    </div>
+  );
+}
