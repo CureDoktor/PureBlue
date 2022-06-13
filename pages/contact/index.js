489badad4bf74aa430160a2562bfa9bbc83ff8ea
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2022-06-13 15:05:38 +0200

    Contact page development

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -62,14 +62,12 @@ export default function Home() {
               <h3>Send A Message</h3>
               <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
                   <Form.Control type="email" placeholder="sample@email.com" />
                   <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                   </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
                   <Form.Control type="text" placeholder="Enter message here." />
                 </Form.Group>
                 <Button variant="primary" type="submit">
