bf71b6002c4d63c2f8449163a0748af1565ca273
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2023-05-05 18:15:12 +0200

    changing phone number

--- pages/terms/index.js
+++ pages/terms/index.js
@@ -799,7 +799,7 @@ export default function Terms() {
             <br />
             Cole Maudlin, Chief Executive Officer; GenRx Pharmacy; 17250 N
             Hartford Dr Ste 115, Scottsdale, AZ 85255; d.maduri@gogomeds.com
-            (email); 1-888-534-8977 (office)
+            (email); 1-866-453-6143 (office)
           </p>
           <h2>29. Contact Us.</h2>
           <p>
