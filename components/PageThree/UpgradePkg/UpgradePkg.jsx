53dabecf66ac4bda5c3ac952dc19a3f51c29a8b2
huzaifa-esspfa <huzaifasiddiqui@esspfa.com>
2024-01-12 18:20:09 +0500

    selected pkg

--- components/PageThree/UpgradePkg/UpgradePkg.jsx
+++ components/PageThree/UpgradePkg/UpgradePkg.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./UpgradePkg.styles.module.scss";
 import { Button, Card, Col, Row } from "react-bootstrap";
+
 const UpgradePkg = () => {
+  const [selectedPackageId, setSelectedPackageId] = useState(2);
+  const checkIcon = "/assets/pageThree/upgradePkg/check.png";
+  const uncheckIcon = "/assets/pageThree/upgradePkg/uncheck.png";
   const packages = [
     {
       id: 1,
@@ -10,7 +14,6 @@ const UpgradePkg = () => {
       doses: `12 doses of 25.0mg Sildenafil (oral - tablet)`,
       savings: "10% Savings ($9.6 discount)",
       additional: "Only pay an additional $86.40",
-      icon: "/assets/pageThree/upgradePkg/uncheck.png",
     },
     {
       id: 2,
@@ -19,8 +22,6 @@ const UpgradePkg = () => {
       doses: "24 doses of 25.0mg Sildenafil (oral - tablet)",
       savings: "20% Savings ($48 discount)",
       additional: "Only pay an additional $192.00",
-      selected: true,
-      icon: "/assets/pageThree/upgradePkg/check.png",
     },
     {
       id: 3,
@@ -29,45 +30,51 @@ const UpgradePkg = () => {
       savings: "30% Savings ($158.4 discount)",
       additional: "Only pay an additional $369.60",
       tBox: "/assets/pageThree/upgradePkg/light.svg",
-      icon: "/assets/pageThree/upgradePkg/uncheck.png",
     },
   ];
+
+  const handleSelectPackage = (id) => {
+    setSelectedPackageId(id);
+  };
+
   return (
     <div className={styles.upgradeSection}>
       <h2>Select your Upgrade Below</h2>
       <Row>
-        {packages.map((pkg) => (
-          <Col key={pkg.id} xs={12} lg={4} md={6}>
-            <Card className={pkg.selected ? styles.selectedCard : styles.card}>
+        {packages.map(({ id, title, doses, savings, additional, tBox }) => (
+          <Col key={id} xs={12} lg={4} md={6}>
+            <Card
+              className={`${styles.card} ${
+                id === selectedPackageId && styles.selectedCard
+              }`}
+              onClick={() => handleSelectPackage(id)}
+            >
               <div className={styles.stepBox}>
-                <img src={pkg.tBox} alt="step" />
-                <p className={pkg.selected ? styles.pColor2 : styles.pColor}>
-                  {pkg.title}
-                </p>
+                <img src={tBox} alt="step" />
+                <p className={styles.pColor}>{title}</p>
               </div>
               <Card.Body className={styles.cardBody}>
-                <div className={styles.listBox}>
-                  <img src={pkg.icon} alt="icon" />
-                  <Card.Text className={pkg.selected && styles.cardTxt}>
-                    {pkg.doses}
-                  </Card.Text>
-                </div>
-
-                <div className={styles.listBox}>
-                  <img src={pkg.icon} alt="icon" />
-                  <Card.Text className={pkg.selected && styles.cardTxt}>
-                    {pkg.savings}
-                  </Card.Text>
-                </div>
-                <div className={styles.listBox}>
-                  <img src={pkg.icon} alt="icon" />
-                  <Card.Text className={pkg.selected && styles.cardTxt}>
-                    {pkg.additional}
-                  </Card.Text>
-                </div>
-
-                <Button className={pkg.selected ? styles.btn2 : styles.btn}>
-                  {pkg.selected ? "SELECTED" : "SELECT"}
+                {[{ text: doses }, { text: savings }, { text: additional }].map(
+                  (item, index) => (
+                    <div className={styles.listBox} key={index}>
+                      <img
+                        src={id === selectedPackageId ? checkIcon : uncheckIcon}
+                        alt="icon"
+                      />
+                      <Card.Text
+                        className={id === selectedPackageId && styles.cardTxt}
+                      >
+                        {item.text}
+                      </Card.Text>
+                    </div>
+                  )
+                )}
+                <Button
+                  className={
+                    id === selectedPackageId ? styles.btn2 : styles.btn
+                  }
+                >
+                  {id === selectedPackageId ? "SELECTED" : "SELECT"}
                 </Button>
               </Card.Body>
             </Card>
