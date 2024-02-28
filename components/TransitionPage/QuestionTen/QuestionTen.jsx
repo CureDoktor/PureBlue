2219253e87fc4cff0f8ed5cfe75423102699c294
Nikola Curcic <nikolacurcic@Nikolas-MacBook-Pro.local>
2024-02-28 19:43:54 +0100

    loading parameters

--- components/TransitionPage/QuestionTen/QuestionTen.jsx
+++ components/TransitionPage/QuestionTen/QuestionTen.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./QuestionTen.styles.module.scss";
-
+import { useEffect } from "react";
+import { Spinner } from "react-bootstrap";
 const QuestionTen = ({ onNext }) => {
+  useEffect(() => {
+    setTimeout(onNext,3000);
+  }, []);
   return (
     <div className={styles.container}>
+      <Spinner animation="border" variant="primary" /> 
       <p>The good news</p>
       <h4>PureBlue can help</h4>
       <img
         src="/assets/transitionPage/heart/Help.png"
         className={styles.image}
-        onClick={onNext}
+     
       />
     </div>
   );
